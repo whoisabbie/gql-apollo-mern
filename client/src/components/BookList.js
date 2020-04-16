7b15b4e98d509481552c787865d0236f2a770130
whoisabbie <44428844+whoisabbie@users.noreply.github.com>
2020-04-16 20:30:36 +0530

    Add files via upload

---
+++ client/src/components/BookList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+
+import { getBooksQuery } from '../queries/queries';
+import BookDetail from './BookDetail';
+
+class BookList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selected: null
+        }
+    }
+
+    displayBooks() {
+        let data = this.props.data;
+        if (data.loading) {
+            return <div>loading books...</div>;
+        } else {
+            return data.books.map(book => {
+                return (
+                    <li key={book.id} onClick={(e) => { this.setState({ selected: book.id }) }}>{book.name}</li>
+                )
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <ul id="book-list">
+                    {this.displayBooks()}
+                </ul>
+                <BookDetail bookId={this.state.selected} />
+            </div>
+        );
+    }
+}
+
+export default graphql(getBooksQuery)(BookList);
